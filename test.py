a9816e8add176ffd711e2c63dc94ab49cfd0bf6b
Richard Khillah <RKhillah@ucla.edu>
2020-04-18 16:07:36 -0700

    add testcase test_avatar

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import unittest
 from app import app, db
 from app.models import User, Post
 
+
 class UserModelCase(unittest.TestCase):
     """docstring for UserModelCase."""
 
@@ -22,7 +23,10 @@ class UserModelCase(unittest.TestCase):
         self.assertTrue(u.check_password('cat'))
 
     def test_avatar(self):
-        pass
+        u = User(username='john', email='john@example.com')
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))
 
     def test_follow(self):
         u1 = User(username='john', email='john@example.com')
