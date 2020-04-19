f2794d14f60bcace422d48471b114922243c0a7c
Richard Khillah <RKhillah@ucla.edu>
2020-04-19 16:21:20 -0700

    squash mistyped keyname bug

--- app/routes.py
+++ app/routes.py
@@ -27,9 +27,9 @@ def index():
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('index', page=posts.next_num) \
-        if posts.has_next else none
+        if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) \
-        if posts.has_prev else none
+        if posts.has_prev else None
     return render_template('index.html',title='Home', form=form,
                             posts=posts.items, next_url=next_url,
                             prev_url=prev_url)
