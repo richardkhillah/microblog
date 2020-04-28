f052f786b6afb77cebf006119a581baf33508986
Richard Khillah <RKhillah@ucla.edu>
2020-04-27 17:08:48 -0700

    refactor main and fix auth e-mail bug

--- app/__init__.py
+++ app/__init__.py
@@ -38,7 +38,10 @@ app.register_blueprint(errors_bp)
 from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix="/auth")
 
-from app import routes, models
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
+
+from app import models
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
