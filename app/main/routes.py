f77458e904f997c0b4fa71b12b9d2ce63a3f42ea
Richard Khillah <RKhillah@ucla.edu>
2020-05-15 00:08:56 -0700

    fix typo in prev_url

--- app/main/routes.py
+++ app/main/routes.py
@@ -128,7 +128,7 @@ def search():
                                 current_app.config['POSTS_PER_PAGE'])
     next_url = url_for('main.search', q=g.search_form.q.data, page=page+1) \
         if total > page * current_app.config['POSTS_PER_PAGE'] else None
-    prev_url = url_for('main.search', q.g.search_form.q.data, page=page-1) \
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page-1) \
         if page > 1 else None
     return render_template('search.html', title=_('Search'), posts=posts,
                             next_url=next_url, prev_url=prev_url)
